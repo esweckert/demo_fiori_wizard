abe35e7dc8b56df87d696f06ecb530e9774ab0ee
Thomas Wecker <thomas.wecker@icloud.com>
2024-07-15 12:21:16 +0200

    initial

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,6 @@
+sap.ui.define([
+	"com/media-saturn/articletransfer/test/unit/controller/App.controller",
+	"com/media-saturn/articletransfer/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
